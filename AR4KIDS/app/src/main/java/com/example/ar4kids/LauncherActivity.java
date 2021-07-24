9929b5e067eba9058197d4c757edc76905dc3b22
Siddhant Dua <siddhantdua@hotmail.com>
2021-07-24 11:41:15 +0530

    App Project Uploaded

---
+++ AR4KIDS/app/src/main/java/com/example/ar4kids/LauncherActivity.java
@@ -0,0 +1,30 @@
+package com.example.ar4kids;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+
+
+        }
+
+
+    public void openar(View view) {
+        Intent intent = new Intent(LauncherActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
